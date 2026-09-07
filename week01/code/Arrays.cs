ffe5d6e1667e8bedc28015949a515f677d46bb93
eneche-web <enechegizo@gmail.com>
2026-09-07 02:40:24 -0700

    Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,8 +15,23 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
 
-        return []; // replace this return statement with your own
+        //plan:
+        // 1. Create an array with spaces enough for the multiples requested
+        // 2. Use a loop to calculate each multiples.
+        // 3. Multiply the given number by the position number plus 1.
+        // 4. Store each multiple in the arrey.
+        // 5. Return the completed arrey.
+
+        double[] multiples = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + !)
+        }
+
+        return  multiples;
     }
 
     /// <summary>
@@ -29,5 +47,25 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Plan:
+        // 1. From where the last "amount" items begins, find the index.
+        // 2. Get the last "amount" items from the list.
+        // 3. From the beginning of the list get the remaining items.
+        // 4. Clear the original list.
+        // 5. To the beginning of the list, add the last items
+        // 6. After the last items, add the remaining items.
+
+        int splitIndex = data.Count - amount;
+
+        List<int> endPart = data.GetRange(splitIndex, amount);
+
+        List<int> beginningPart = data.GetRange(0, splitIndex);
+
+        data.Clear();
+
+        data.AddRange(endPart);
+
+        data.AddRange(beginningPart);
     }
 }
